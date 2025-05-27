3d5e18e475b007d4228d8bd69550e6507ff6a5f1
Ruba-Salih <rubasalih900@gmail.com>
2025-05-28 00:14:49 +0300

    database

--- graduation_projects_management/settings.py
+++ graduation_projects_management/settings.py
@@ -133,16 +133,13 @@ LOGGING = {
  """
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
+import dj_database_url
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': config('DB_NAME'),
-        'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PASSWORD'),
-        'HOST': config('DB_HOST'),
-        'PORT': config('DB_PORT'),
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
 
 
